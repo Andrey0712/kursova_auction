f7f51e6d32b698bc9f1d957af4a35e03e2eab52e
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-05-31 16:00:55 +0300

    Start

---
+++ WebAuction/WebAuction/Controllers/LotController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAuction.Model;
+
+namespace WebAuction.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LotController : ControllerBase
+    {
+        [HttpGet]
+        [Route("search")]
+        public IActionResult Search()
+        {
+            var list = new List<LotVM>()
+            {
+
+                new LotVM
+                {
+                Name = "Манета 1 рубль",
+                Prise = 500,
+                Description = "1 рубль 1882 г. в идеальном состоянии",
+                Image = "1.png"
+                },
+                new LotVM
+                {
+                Name = "Картина К.Малевича",
+                Prise = 50000,
+                Description = "Оригинал",
+                Image = "2.png"
+                },
+            };
+            return Ok(list);
+        }
+    }
+}
---
+++ WebAuction/WebAuction/Model/LotVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAuction.Model
+{
+    public class LotVM
+    {
+        public string Name { get; set; }
+        public int Prise { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
